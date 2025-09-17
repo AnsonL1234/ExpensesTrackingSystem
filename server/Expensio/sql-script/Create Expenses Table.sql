968e2a5b5c865a10d67f95020c0f8170417cfbeb
AnsonL1234 <ansontan1001@gmail.com>
2025-09-17 11:19:03 +0100

    adding new script for sql

---
+++ server/Expensio/sql-script/Create Expenses Table.sql
@@ -0,0 +1,7 @@
+create table expenses(
+	expenses_id int auto_increment not null primary key, 
+    user_id int not null, 
+    amount double not null, 
+    purpose varchar(25) not null, 
+    spend_at datetime not null
+);
